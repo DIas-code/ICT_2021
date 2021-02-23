7529382af2a6d7fed738ca7c5aaafcf492eee3bb
Dias <diasermekov09@gmail.com>
2021-02-24 01:35:04 +0600

    Task3

--- task3/9.py
+++ task3/9.py
@@ -1,2 +1,11 @@
-dec=int(input("Decimal number: "))
-print("Binary: ", bin(dec))
+def dtb(num,result):
+    if num>=1:
+        a=num%2
+        r=str(a)
+        result+=r
+        dtb(num//2,result)
+    else: print(result)
+
+num=int(input("Decimal num: " ))
+result=""
+dtb(num,result)
