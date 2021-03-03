68eb3481ce2767e7ef600dde5610c8faa7f6ebf4
Dias <diasermekov09@gmail.com>
2021-03-03 18:26:58 +0600

    task4

--- task4/7.py
+++ task4/7.py
@@ -1,6 +1,5 @@
 import random
-def Gp(a):
-    
+def Gp(a,cnt):
     GP=""
     for _ in range (0,a):
         L=random.randint(33,126)
@@ -16,9 +15,12 @@ def Gp(a):
         elif i>="0" and i<="9":
             isnum=True
     if isnum==True and islow==True and isup==True:
+        print("The number of attempts",cnt)
         return GP
+        
     else:
-        return Gp(a)
+        cnt+=1
+        return Gp(a,cnt)
 a=random.randint(8,20)
 print(a)
-print("Random good password",Gp(a))
+print("Random good password",Gp(a,0))
