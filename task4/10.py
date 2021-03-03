4b6da5d800e99132c5dd917f5e4269f742a852c3
Dias <diasermekov09@gmail.com>
2021-03-03 19:40:25 +0600

    task4

--- task4/10.py
+++ task4/10.py
@@ -1,6 +1,18 @@
-import datetime
-from datetime import timedelta 
-date = input()
-currdate = datetime.to_datetime(date)
-nextday = currdate + timedelta(days = 1)
-print(nextday)
+def is_magic_date(d,m,y):
+    if d*m==y%100:
+        return True
+    else: return False
+
+
+days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+for year in range (1900,2000):
+    for month in range (1,13):
+        if year%4==0:
+            days[2]=29
+        else:
+            days[2]=28
+        for day in range(1,days[month]+1):
+            if is_magic_date(day,month,year)==True:
+
+                print("%02d:%02d:{} is magic date".format(year)%(day,month))
+
